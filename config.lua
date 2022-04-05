553d719cd8cf85796ce17c92d4ecd22cbc0002a3
Vu Tran <baysao@gmail.com>
2022-04-05 14:00:37 +0700

    Add config.lua

--- config.lua
+++ config.lua
@@ -7,7 +7,15 @@ local _config = {
         }
     },
     templates = {},
-    apps = {
+    apps = {},
+    supervisors = {
+        ["monitor_client"] = [[
+[program:monitor_client]
+command=/bin/bash _SITE_ROOT_/etc/mkagent/agents/push.sh _SITE_ROOT_
+autorestart=true
+redirect_stderr=true
+stdout_logfile=_SITE_ROOT_/logs/monitor_client.log
+    ]]
     },
     supervisor = [[
 
