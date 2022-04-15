72468183b3a30b4959686d1294a2270f21e2a3d4
Vu Tran <baysao@gmail.com>
2022-04-15 14:12:06 +0700

    Add create pass for repo

--- scripts/repo.sh
+++ scripts/repo.sh
@@ -1,7 +1,40 @@
 #!/bin/bash
-SITE_ROOT=$(dirname $(realpath $0))
+SITE_ROOT=$(realpath $(dirname $(realpath $0))/..)
 cd $SITE_ROOT
-_add() {
+
+#users=(session env api asdf gateway gbc git gwman mkagent monitor node ssl stat apideploy ssh  gwmandeploy statdeploy )
+_passwd() {
+	pass=$(
+		tr </dev/urandom -dc _A-Z-a-z-0-9 | head -c${1:-32}
+		echo
+	)
+	echo $pass
+}
+_repo_passwd() {
+	_repo=$1
+	_pass=$(_passwd)
+	_massbit_user=$(cat data/.public_user)
+	_massbit_pwd=$(cat data/.public_pass)
+	echo $_repo $_pass >>data/.passwords
+	htpasswd -bc "data/${_repo}_write.htpasswd" "$_repo" "$_pass"
+	htpasswd -bc "data/${_repo}.htpasswd" "$_repo" "$_pass"
+	htpasswd -b "data/${_repo}.htpasswd" "$_massbit_user" "$massbit_pwd"
+
+	# massbit_pwd=$(echo massbit123 | sha1sum | sed 's/  -//')
+	# for user in "${users[@]}"; do
+	# 	echo $user
+	# 	password=$(echo ${user}${user} | sha1sum | sed 's/  -//')
+	# 	htpasswd -bc "${user}_write.htpasswd" "$user" "$password"
+	# 	htpasswd -bc "$user.htpasswd" "$user" "$password"
+	# 	htpasswd -b "$user.htpasswd" massbit "$massbit_pwd"
+	# done
+}
+_repos_create() {
+	_repo_list=(session env api asdf gateway gbc git gwman mkagent monitor node ssl stat apideploy ssh gwmandeploy statdeploy)
+
+}
+
+_repo_add() {
 	_repo_name=$1
 	mkdir -p $_repo_name
 	cd $_repo_name
@@ -10,3 +43,4 @@ _add() {
 	chown -R www-data.www-data .
 	chmod -R 777 .
 }
+$@
