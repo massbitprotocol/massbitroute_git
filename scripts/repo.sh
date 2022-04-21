9f1f268584877db6f8971002384bfbe9acab00c2
Vu Tran <baysao@gmail.com>
2022-04-21 13:58:36 +0700

    Update repo script

--- scripts/repo.sh
+++ scripts/repo.sh
@@ -2,7 +2,7 @@
 SITE_ROOT=$(realpath $(dirname $(realpath $0))/..)
 cd $SITE_ROOT
 
-#users=(session env api asdf gateway gbc git gwman mkagent monitor node ssl stat apideploy ssh  gwmandeploy statdeploy )
+repos=(env ssl gitdeploy apideploy gwmandeploy statdeploy)
 _passwd() {
 	pass=$(
 		tr </dev/urandom -dc _A-Z-a-z-0-9 | head -c${1:-32}
@@ -10,37 +10,33 @@ _passwd() {
 	)
 	echo $pass
 }
-_repo_passwd() {
-	_repo=$1
+_get_passwd_default() {
+	_user="massbit"
 	_pass=$(_passwd)
-	_massbit_user=$(cat data/.public_user)
-	_massbit_pwd=$(cat data/.public_pass)
-	echo $_repo $_pass >>data/.passwords
+	echo $_user $_pass >data/default.user
+}
+_get_passwd() {
+	_repo=$1
+	if [ -f "data/${_repo}.user" ]; then return; fi
+	_pass=$(_passwd)
+
+	echo "$_repo $_pass" >data/${_repo}.user
 	htpasswd -bc "data/${_repo}_write.htpasswd" "$_repo" "$_pass"
 	htpasswd -bc "data/${_repo}.htpasswd" "$_repo" "$_pass"
-	htpasswd -b "data/${_repo}.htpasswd" "$_massbit_user" "$massbit_pwd"
-
-	# massbit_pwd=$(echo massbit123 | sha1sum | sed 's/  -//')
-	# for user in "${users[@]}"; do
-	# 	echo $user
-	# 	password=$(echo ${user}${user} | sha1sum | sed 's/  -//')
-	# 	htpasswd -bc "${user}_write.htpasswd" "$user" "$password"
-	# 	htpasswd -bc "$user.htpasswd" "$user" "$password"
-	# 	htpasswd -b "$user.htpasswd" massbit "$massbit_pwd"
-	# done
+	htpasswd -b "data/${_repo}.htpasswd" $(cat data/default.user)
 }
 _repos_create() {
-	_repo_list=(session env api asdf gateway gbc git gwman mkagent monitor node ssl stat apideploy ssh gwmandeploy statdeploy)
-
+	for _repo in $repos; do
+		_get_passwd $_repo
+	done
 }
 
 _repo_add() {
 	_repo_name=$1
-	mkdir -p $_repo_name
-	cd $_repo_name
+	mkdir -p data/repo/massbitroute/$_repo_name
 	git --bare init
 	git update-server-info
-	chown -R www-data.www-data .
-	chmod -R 777 .
+	chown -R www-data.www-data data/repo/massbitroute/$_repo_name
+	chmod -R 777 data/repo/massbitroute/$_repo_name
 }
 $@
