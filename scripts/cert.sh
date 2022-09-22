c7fd78454b66f7eb91b7e83aa1224f5ea74bcdaf
Vu Tran <baysao@gmail.com>
2022-09-22 13:42:32 +0700

    chore: update multi domain for gateway

--- scripts/cert.sh
+++ scripts/cert.sh
@@ -12,11 +12,19 @@ _install() {
 _get() {
 	domain=$1
 	email=product@massbit.io
-	certbot certonly --non-interactive --agree-tos  -m $email --manual --manual-auth-hook /etc/letsencrypt/acme-dns-auth.py --manual-public-ip-logging-ok --preferred-challenges dns --debug-challenges -d \*.$domain -d $domain
+	mkdir -p /tmp/ssl
+	certbot certonly --non-interactive --agree-tos -m $email --manual --manual-auth-hook /etc/letsencrypt/acme-dns-auth.py --manual-public-ip-logging-ok --preferred-challenges dns --debug-challenges -d \*.$domain -d $domain >/tmp/ssl/$domain
 }
 _get_mydomain() {
 	domain=$1
 	_get $domain
+	_get bsc-mainnet.$domain
+	_get matic-mainnet.$domain
+	_get matic-testnet.$domain
+	_get eth-mainnet.$domain
+	_get eth-rinkeby.$domain
+	_get dot-mainnet.$domain
+	cat /tmp/ssl/* | awk '/_acme-challenge.* CNAME .*.auth.acme-dns.io./'
 }
 _renew() {
 	certbot renew
