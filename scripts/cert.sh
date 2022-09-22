d9d2a4e0bf48bb8b1af0bf72f7a2a016d35dfc5f
Vu Tran <baysao@gmail.com>
2022-09-22 11:44:27 +0700

    chore: add get all subdomain

--- scripts/cert.sh
+++ scripts/cert.sh
@@ -7,11 +7,15 @@ _install() {
 	# wget https://github.com/joohoi/acme-dns-certbot-joohoi/raw/master/acme-dns-auth.py
 	##!/usr/bin/env python3
 	chmod +x $dir/acme-dns-auth.py
-	mv acme-dns-auth.py /etc/letsencrypt/
+	cp $dir/acme-dns-auth.py /etc/letsencrypt/
 }
 _get() {
 	domain=$1
 	certbot certonly --manual --manual-auth-hook /etc/letsencrypt/acme-dns-auth.py --preferred-challenges dns --debug-challenges -d \*.$domain -d $domain
+}
+_get_mydomain(){
+    domain=$1
+    _get $domain
 }
 _renew() {
 	certbot renew
