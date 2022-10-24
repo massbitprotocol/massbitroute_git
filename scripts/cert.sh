2468980378df2582efb5daab5dd2108f0438f51d
Vu Tran <baysao@users.noreply.github.com>
2022-10-24 15:01:10 +0700

    Release (#16)
    * Hotfix certs (#11)
    
    * chore: add install certbot
    
    * chore: add get all subdomain
    
    * chore: install without interaction
    
    * chore: auto ok with log ip
    
    * chore: update multi domain for gateway
    
    * chore: update multi domain for gateway
    
    * chore: update git ssl for gateway
    
    * fix: wrong order command
    
    * fix: wrong dir letscrypt
    
    * Hotfix certs (#12)
    
    * chore: add install certbot
    
    * chore: add get all subdomain
    
    * chore: install without interaction
    
    * chore: auto ok with log ip
    
    * chore: update multi domain for gateway
    
    * chore: update multi domain for gateway
    
    * chore: update git ssl for gateway
    
    * fix: wrong order command
    
    * fix: wrong dir letscrypt
    
    * chore: add _renew cert to main flow
    
    * Hotfix certs (#13)
    
    * chore: add install certbot
    
    * chore: add get all subdomain
    
    * chore: install without interaction
    
    * chore: auto ok with log ip
    
    * chore: update multi domain for gateway
    
    * chore: update multi domain for gateway
    
    * chore: update git ssl for gateway
    
    * fix: wrong order command
    
    * fix: wrong dir letscrypt
    
    * chore: add _renew cert to main flow
    
    * remove repo_init
    
    * Hotfix certs (#14)
    
    * chore: add install certbot
    
    * chore: add get all subdomain
    
    * chore: install without interaction
    
    * chore: auto ok with log ip
    
    * chore: update multi domain for gateway
    
    * chore: update multi domain for gateway
    
    * chore: update git ssl for gateway
    
    * fix: wrong order command
    
    * fix: wrong dir letscrypt
    
    * chore: add _renew cert to main flow
    
    * remove repo_init
    
    * chore: make exec permission
    
    * fix: check timestamp for ssl gateway archive (#15)
    
    * support upgrade by tag
    
    * add MONITOR_SCHEME
    
    * add TYPE for monitor_client
    
    * chore: ignore `vars/ID`
    
    * chore: auto push private repo
    
    * add update core repo

--- scripts/cert.sh
+++ scripts/cert.sh
@@ -1,17 +1,53 @@
 #!/bin/bash
+dir=$(dirname $(realpath $0))
 _install() {
+	apt update
 	#apt-add-repository ppa:certbot/certbot
 	apt install -y certbot
-	wget https://github.com/joohoi/acme-dns-certbot-joohoi/raw/master/acme-dns-auth.py
+	# wget https://github.com/joohoi/acme-dns-certbot-joohoi/raw/master/acme-dns-auth.py
 	##!/usr/bin/env python3
-	chmod +x acme-dns-auth.py
-	mv acme-dns-auth.py /etc/letsencrypt/
+	chmod +x $dir/acme-dns-auth.py
+	cp $dir/acme-dns-auth.py /etc/letsencrypt/
 }
 _get() {
 	domain=$1
-	certbot certonly --manual --manual-auth-hook /etc/letsencrypt/acme-dns-auth.py --preferred-challenges dns --debug-challenges -d \*.$domain -d $domain
+	email=product@massbit.io
+	mkdir -p /tmp/ssl
+	certbot certonly --non-interactive --agree-tos -m $email --manual --manual-auth-hook /etc/letsencrypt/acme-dns-auth.py --manual-public-ip-logging-ok --preferred-challenges dns --debug-challenges -d \*.$domain -d $domain >/tmp/ssl/$domain
+}
+_get_mydomain() {
+	domain=$1
+	_get $domain
+}
+_prepare_gateway() {
+	if [ -f "/tmp/ssl_time" ]; then
+		t0=$(cat /tmp/ssl_time)
+	else
+		t0=0
+	fi
+	_now=$(date +%Y%m%d)
+	if [ $_now -eq $t0 ]; then return; fi
+
+	echo $_now >/tmp/ssl_time
+
+	ssldir=/massbit/massbitroute/app/src/sites/services/git/data/ssl
+	tmpd=$(mktemp -d)
+	mkdir -p $tmpd/{live,archive}
+	cp -rf /etc/letsencrypt/archive/{eth,matic}* $tmpd/archive/
+	cp -rf /etc/letsencrypt/live/{eth,matic}* $tmpd/live/
+	cd $tmpd
+	tar -cvzf gateway_ssl.tar.gz *
+
+	cp $tmpd/gateway_ssl.tar.gz $ssldir
+	cd $ssldir
+	git add gateway_ssl.tar.gz
+	git commit -m "$(date) update ssl"
+	git push
+	rm -rf $tmpd
+
 }
 _renew() {
 	certbot renew
+	_prepare_gateway
 }
 $@
